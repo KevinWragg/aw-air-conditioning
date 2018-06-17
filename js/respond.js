12b0be9980458c2b246522cadc292a7f736ce229
Kevin Wragg <kevinwragg.dev@gmail.com>
2018-06-17 17:30:20 +0100

    Prototype
    Requires support for ie11, safari..

---
+++ js/respond.js
@@ -0,0 +1,50 @@
+var intro = document.querySelector('.intro');
+var servicesRow = document.querySelector('.row');
+var servicesCol = document.querySelectorAll('.col-1-of-3');
+var footerRow = document.querySelector('.row');
+
+
+function adjustIntro(){
+   if(window.innerWidth <= 1352){
+      intro.classList.remove("margin-top-medium");
+   }
+   else{
+      intro.classList.add("margin-top-medium");
+   }
+}
+
+function adjustServicesRow(){
+   if(window.innerWidth <= 900){
+      servicesRow.classList.remove("row");
+   }
+   else{
+      servicesRow.classList.add("row");
+   }
+}
+
+function adjustServicesCol(){
+   for(i = 0; i < servicesCol.length; i++){
+      if(window.innerWidth <= 900){
+         servicesCol[i].classList.remove("col-1-of-3");
+      }
+      else{
+         servicesCol[i].classList.add("col-1-of-3");
+      }
+   }
+}
+
+function adjustFooterRow(){
+   if(window.innerWidth <= 900){
+      footerRow.classList.remove("row");
+   }
+   else{
+      footerRow.classList.add("row");
+   }
+}
+
+
+
+setInterval(adjustIntro, 50);
+setInterval(adjustServicesCol, 50);
+setInterval(adjustServicesRow, 50);
+setInterval(adjustFooterRow, 50);
